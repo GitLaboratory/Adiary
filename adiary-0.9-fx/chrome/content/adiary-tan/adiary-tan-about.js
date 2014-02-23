2b7536e43b1c9f83dc622947d5ed626984fdf68d
Il'ya A. Lykov <lykov1992@gmail.com>
2014-02-23 07:58:06 +0700

    Adiary

--- adiary-0.9-fx/chrome/content/adiary-tan/adiary-tan-about.js
+++ adiary-0.9-fx/chrome/content/adiary-tan/adiary-tan-about.js
@@ -1,109 +1,102 @@
 var adiaryTanStringBundle = {
-  _bundle: Components.classes["@mozilla.org/intl/stringbundle;1"]
-             .getService(Components.interfaces.nsIStringBundleService)
-             .createBundle("chrome://adiary-tan/locale/adiary-tan.properties"),
+	_bundle : Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService).createBundle("chrome://adiary-tan/locale/adiary-tan.properties"),
 
-  getString: function(aMessage, aArgs) {
-    if (aArgs) {
-      aArgs = [].slice.call(arguments, 1);
-      return this._bundle.formatStringFromName(aMessage, aArgs, aArgs.length);
-    } else {
-      return this._bundle.GetStringFromName(aMessage);
-    }
-  }
+	getString : function(aMessage, aArgs) {
+		if (aArgs) {
+			aArgs = [].slice.call(arguments, 1);
+			return this._bundle.formatStringFromName(aMessage, aArgs, aArgs.length);
+		} else {
+			return this._bundle.GetStringFromName(aMessage);
+		}
+	}
 };
 
 var adiaryTanAbout = {
-  changeStyle: function(aBox, aBalloon, aLabel1, aLabel2, aLabel3,
-                        aType, aArgs) {
-    var propertyName = "adiaryTanAbout.type" + aType;
-    var backgroundImageValue = adiaryTanStringBundle
-                                 .getString(propertyName + ".backgroundImage");
-    var labelValue;
+	changeStyle : function(aBox, aBalloon, aLabel1, aLabel2, aLabel3, aType, aArgs) {
+		var propertyName = "adiaryTanAbout.type" + aType;
+		var backgroundImageValue = adiaryTanStringBundle.getString(propertyName + ".backgroundImage");
+		var labelValue;
 
-    if (aArgs) {
-      aArgs = [].slice.call(arguments, 6);
-      aArgs.unshift(propertyName + ".label");
-      labelValue = adiaryTanStringBundle.getString
-                                        .apply(adiaryTanStringBundle, aArgs);
-    } else {
-      labelValue = adiaryTanStringBundle.getString(propertyName + ".label");
-    }
+		if (aArgs) {
+			aArgs = [].slice.call(arguments, 6);
+			aArgs.unshift(propertyName + ".label");
+			labelValue = adiaryTanStringBundle.getString.apply(adiaryTanStringBundle, aArgs);
+		} else {
+			labelValue = adiaryTanStringBundle.getString(propertyName + ".label");
+		}
 
-    if (backgroundImageValue) {
-      aBox.setAttribute("style", backgroundImageValue);
-    }
-    if (labelValue) {
-      var labelArray = labelValue.split("\n");
-      if (labelArray[0] == "") {
-        aLabel1.setAttribute("hidden", true);
-      } else {
-        aLabel1.removeAttribute("hidden");
-        aLabel1.value = labelArray[0];
-      }
-      if (labelArray[1] == "") {
-        aLabel2.setAttribute("hidden", true);
-      } else {
-        aLabel2.removeAttribute("hidden");
-        aLabel2.value = labelArray[1];
-      }
-      if (labelArray[2] == "") {
-        aLabel3.setAttribute("hidden", true);
-      } else {
-        aLabel3.removeAttribute("hidden");
-        aLabel3.value = labelArray[2];
-      }
-    }
-  },
+		if (backgroundImageValue) {
+			aBox.setAttribute("style", backgroundImageValue);
+		}
+		if (labelValue) {
+			var labelArray = labelValue.split("\n");
+			if (labelArray[0] == "") {
+				aLabel1.setAttribute("hidden", true);
+			} else {
+				aLabel1.removeAttribute("hidden");
+				aLabel1.value = labelArray[0];
+			}
+			if (labelArray[1] == "") {
+				aLabel2.setAttribute("hidden", true);
+			} else {
+				aLabel2.removeAttribute("hidden");
+				aLabel2.value = labelArray[1];
+			}
+			if (labelArray[2] == "") {
+				aLabel3.setAttribute("hidden", true);
+			} else {
+				aLabel3.removeAttribute("hidden");
+				aLabel3.value = labelArray[2];
+			}
+		}
+	},
 
-  react: function() {
-    var box = document.getElementById("clientBox");
-    var balloon = document.getElementById("adiaryTanBalloon");
-    var label1 = document.getElementById("adiaryTanLabel1");
-    var label2 = document.getElementById("adiaryTanLabel2");
-    var label3 = document.getElementById("adiaryTanLabel3");
+	react : function() {
+		var box = document.getElementById("clientBox");
+		var balloon = document.getElementById("adiaryTanBalloon");
+		var label1 = document.getElementById("adiaryTanLabel1");
+		var label2 = document.getElementById("adiaryTanLabel2");
+		var label3 = document.getElementById("adiaryTanLabel3");
 
-    if (box && balloon) {
-      if (box.getAttribute("adiaryTanClicked") == "true") {
-        this.changeStyle(box, balloon, label1, label2, label3, 0);
-        balloon.setAttribute("hidden", true);
-        box.setAttribute("adiaryTanClicked", false);
-      } else {
-        if (Math.random() > 0.6) {
-          switch (Math.floor(Math.random() * 3)) {
-            // Wafu-!
-            case 0:
-              this.changeStyle(box, balloon, label1, label2, label3, 2);
-              break;
-            // Judgement desuno!
-            case 1:
-              this.changeStyle(box, balloon, label1, label2, label3, 3);
-              break;
-            // Akkari-n
-            case 2:
-              this.changeStyle(box, balloon, label1, label2, label3, 4);
-              break;
-          }
-        } else {
-          // Do you have the time?
-          var nowDate = new Date();
-          var nowHour = nowDate.getHours().toString();
-          var nowMinute = nowDate.getMinutes().toString();
-          if (nowMinute.length == 1) {
-            nowMinute = "0" + nowMinute;
-          }
-          this.changeStyle(box, balloon, label1, label2, label3, 1,
-                           nowHour, nowMinute);
-        }
-        balloon.removeAttribute("hidden");
-        box.setAttribute("adiaryTanClicked", true);
-      }
-    }
-  },
+		if (box && balloon) {
+			if (box.getAttribute("adiaryTanClicked") == "true") {
+				this.changeStyle(box, balloon, label1, label2, label3, 0);
+				balloon.setAttribute("hidden", true);
+				box.setAttribute("adiaryTanClicked", false);
+			} else {
+				if (Math.random() > 0.6) {
+					switch (Math.floor(Math.random() * 3)) {
+						// Wafu-!
+						case 0:
+							this.changeStyle(box, balloon, label1, label2, label3, 2);
+							break;
+						// Judgement desuno!
+						case 1:
+							this.changeStyle(box, balloon, label1, label2, label3, 3);
+							break;
+						// Akkari-n
+						case 2:
+							this.changeStyle(box, balloon, label1, label2, label3, 4);
+							break;
+					}
+				} else {
+					// Do you have the time?
+					var nowDate = new Date();
+					var nowHour = nowDate.getHours().toString();
+					var nowMinute = nowDate.getMinutes().toString();
+					if (nowMinute.length == 1) {
+						nowMinute = "0" + nowMinute;
+					}
+					this.changeStyle(box, balloon, label1, label2, label3, 1, nowHour, nowMinute);
+				}
+				balloon.removeAttribute("hidden");
+				box.setAttribute("adiaryTanClicked", true);
+			}
+		}
+	},
 
-  openWebsite: function(aURL) {
-    document.getElementById("aboutDialog").acceptDialog();
-    window.opener.getBrowser().selectedTab = window.opener.getBrowser()
-                                                          .addTab(aURL);
-  }
+	openWebsite : function(aURL) {
+		document.getElementById("aboutDialog").acceptDialog();
+		window.opener.getBrowser().selectedTab = window.opener.getBrowser().addTab(aURL);
+	}
 };
--- adiary-0.9-fx/chrome/content/adiary-tan/adiary-tan-status.js
+++ adiary-0.9-fx/chrome/content/adiary-tan/adiary-tan-status.js
@@ -1,76 +1,75 @@
 var adiaryTanProgressListener = {
-  QueryInterface: function(aIID) {
-    if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
-        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
-        aIID.equals(Components.interfaces.nsISupports)) {
-      return this;
-    }
-    throw Components.results.NS_NOINTERFACE;
-  },
+	QueryInterface : function(aIID) {
+		if (aIID.equals(Components.interfaces.nsIWebProgressListener) || aIID.equals(Components.interfaces.nsISupportsWeakReference) || aIID.equals(Components.interfaces.nsISupports)) {
+			return this;
+		}
+		throw Components.results.NS_NOINTERFACE;
+	},
 
-  onStateChange: function(aProgress, aRequest, aFlag, aStatus) {
-    if (aFlag & Components.interfaces.nsIWebProgressListener.STATE_STOP &&
-        aFlag & Components.interfaces.nsIWebProgressListener.STATE_IS_NETWORK) {
-      adiaryTanStatus.showStatus();
-    }
-  },
+	onStateChange : function(aProgress, aRequest, aFlag, aStatus) {
+		if (aFlag & Components.interfaces.nsIWebProgressListener.STATE_STOP && aFlag & Components.interfaces.nsIWebProgressListener.STATE_IS_NETWORK) {
+			adiaryTanStatus.showStatus();
+		}
+	},
 
-  onLocationChange: function(aProgress, aRequest, aURI) {
-    adiaryTanStatus.showStatus();
-  },
+	onLocationChange : function(aProgress, aRequest, aURI) {
+		adiaryTanStatus.showStatus();
+	},
 
-  onProgressChange: function() {},
-  onStatusChange: function() {},
-  onSecurityChange: function() {},
-  onLinkIconAvailable: function() {}
+	onProgressChange : function() {
+	},
+	onStatusChange : function() {
+	},
+	onSecurityChange : function() {
+	},
+	onLinkIconAvailable : function() {
+	}
 };
 
 var adiaryTanStatus = {
-  init: function() {
-    // Listen for webpage loads
-    gBrowser.addProgressListener(adiaryTanProgressListener,
-                                 Components.interfaces.nsIWebProgress
-                                                      .NOTIFY_STATE_DOCUMENT);
-  },
-  
-  uninit: function() {
-    gBrowser.removeProgressListener(adiaryTanProgressListener);
-  },
+	init : function() {
+		// Listen for webpage loads
+		gBrowser.addProgressListener(adiaryTanProgressListener, Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+	},
 
-  showStatus: function() {
-    if (typeof(content.document) == "undefined") {
-      return;
-    }
+	uninit : function() {
+		gBrowser.removeProgressListener(adiaryTanProgressListener);
+	},
 
-    var metaCollection = content.document.getElementsByTagName("meta");
-    var panel = document.getElementById("adiaryTanStatus");
-    var isAdiary = false;
-    var adiaryVersion = "";
+	showStatus : function() {
+		if ( typeof (content.document) == "undefined") {
+			return;
+		}
 
-    if (panel) {
-      // Is this page made by adiary?
-      for (var i = 0; i < metaCollection.length; i++) {
-        if (metaCollection[i].name.search(/^generator$/) != -1 &&
-            metaCollection[i].content.search(/^adiary/) != -1) {
-          isAdiary = true;
-          adiaryVersion = metaCollection[i].content.replace(/^adiary /, "");
-        }
-      }
+		var metaCollection = content.document.getElementsByTagName("meta");
+		var panel = document.getElementById("adiaryTanStatus");
+		var isAdiary = false;
+		var adiaryVersion = "";
 
-      // Modify status
-      if (isAdiary) {
-        panel.setAttribute("tooltiptext", adiaryVersion);
-        panel.removeAttribute("hidden");
-      } else {
-        panel.setAttribute("hidden", true);
-      }
-    }
+		if (panel) {
+			// Is this page made by adiary?
+			for (var i = 0; i < metaCollection.length; i++) {
+				if (metaCollection[i].name.search(/^generator$/) != -1 && metaCollection[i].content.search(/^adiary/) != -1) {
+					isAdiary = true;
+					adiaryVersion = metaCollection[i].content.replace(/^adiary /, "");
+				}
+			}
 
-    return;
-  }
+			// Modify status
+			if (isAdiary) {
+				panel.setAttribute("tooltiptext", adiaryVersion);
+				panel.removeAttribute("hidden");
+			} else {
+				panel.setAttribute("hidden", true);
+			}
+		}
+		return;
+	}
 };
 
-window.addEventListener("load", function() { adiaryTanStatus.init() },
-                        false);
-window.addEventListener("unload", function() { adiaryTanStatus.uninit() },
-                        false);
+window.addEventListener("load", function() {
+	adiaryTanStatus.init()
+}, false);
+window.addEventListener("unload", function() {
+	adiaryTanStatus.uninit()
+}, false);
